687d122abbfb789f66f9fa89ce462d8315c11066
tenisistaa <krzychu_ten@o2.pl>
2018-07-20 01:13:09 +0200

    local storage

--- src/App.js
+++ src/App.js
@@ -7,12 +7,62 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      forms: [
-        { id: 1, formType: 1, selectType: null, depth: 0},
-        { id: 2, formType: 2, selectType: 'Yes/No', depth: 1},
-        { id: 3, formType: 1, selectType: null, depth: 0}
-      ],
+      forms: [],
     };
+  }
+
+  componentDidMount() {
+    this.hydrateStateWithLocalStorage();
+
+    // add event listener to save state to localStorage
+    // when user leaves/refreshes the page
+    window.addEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+
+    // saves if component has a chance to unmount
+    this.saveStateToLocalStorage();
+  }
+
+  hydrateStateWithLocalStorage() {
+    // for all items in state
+    for (let key in this.state) {
+      // if the key exists in localStorage
+      if (localStorage.hasOwnProperty(key)) {
+        // get the key's value from localStorage
+        let value = localStorage.getItem(key);
+
+        // parse the localStorage string and setState
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          // handle empty string
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  }
+
+  saveStateToLocalStorage() {
+    // for every item in React state
+    for (let key in this.state) {
+      // save to localStorage
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
+
+  updateInput(key, value) {
+    // update react state
+    this.setState({ [key]: value });
   }
 
   addForm = (id, formType, selectType, depth) => {
